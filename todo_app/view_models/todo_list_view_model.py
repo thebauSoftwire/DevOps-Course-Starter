dea24dcf9208df8baeef71f5d0ffbd453ee79f23
Thea Bautista <thea.bautista@softwire.com>
2024-03-15 17:01:19 +0000

    Feature: add functionality for returning done and to do items

--- todo_app/view_models/todo_list_view_model.py
+++ todo_app/view_models/todo_list_view_model.py
@@ -8,4 +8,10 @@ class TodoListViewModel:
     
     @property
     def done_items(self):
-        return[]
+        complete_items = filter(lambda item: item['status'] == 'Done', self.items)
+        return list(complete_items)
+    
+    @property
+    def to_do_items(self):
+        to_do_items = filter(lambda item: item['status'] == 'To Do', self.items)
+        return list(to_do_items)
